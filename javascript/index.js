897ad2707203b78c06bcca039667c552621c78ea
luan <luanakarolineliramateus@outlook.com>
2023-12-04 16:20:17 -0300

    primeiro commit

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('blur',() =>{
+    document.title = 'sentimos sua falta...'
+})
+
+window.addEventListener('focus',() =>{
+  document.title =  "AnimeMania"
+})
+
+var peagr=""
+var divNova = document.createElement("div");
+function chamar(){
+    peagr = document.getElementById("textareamensagem").value
+    document.getElementById('digitar').innerHTML = peagr;
+    
+
+}
